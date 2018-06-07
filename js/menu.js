081223c0b5aa3722d2ba058bb063f1f48ea5cb15
kapiszon1999 <32983854+kapiszon1999@users.noreply.github.com>
2018-06-07 12:54:58 +0200

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 let menuShowed = false;
 let $menu = document.querySelector(".menu");
 let $burger = document.querySelector(".burger");
+let $burgerWrapp = document.querySelector(".burger-wrapper");
 let position = 0;
 
 document.querySelector(".burger").addEventListener("click", showMenu)
@@ -8,6 +9,7 @@ document.querySelector(".burger").addEventListener("click", showMenu)
 function showMenu() {
     position = window.pageYOffset;
     if (menuShowed == false) {
+        $burgerWrapp.classList.add(".burger-rotated");
         $menu.style.top = window.scrollY + "px";
         menuShowed = true;
         document.addEventListener("scroll", blocked);
@@ -16,6 +18,7 @@ function showMenu() {
         document.querySelector(".wing:nth-child(3)").classList.add("lower-wing");
         $menu.classList.add("m-showed");
     } else {
+        $burgerWrapp.classList.remove(".burger-rotated");
         document.removeEventListener("scroll", blocked);
         document.querySelector(".wing:nth-child(1)").classList.remove("upper-wing");
         document.querySelector(".wing:nth-child(2)").classList.remove("middle-wing");
@@ -30,25 +33,25 @@ function blocked(e) {
     window.scrollTo(0, position);
 };
 
-let words = [];
-let $menuBtns = document.querySelectorAll(".bt-back");
-let wordsCount = () => {
-    $menuBtns.forEach((item, ind) => {
-        words[ind] = item.textContent;
-    })
-}
-wordsCount();
-window.addEventListener("resize", menuSetup);
-
-function menuSetup() {
-    let style = window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('font-size');
-    let fontSize = parseFloat(style);
-    $menuBtns.forEach((item, ind) => {
-        let itemWidth = words[ind].length * fontSize * 3;
-        itemWidth = itemWidth - itemWidth * 0.45;
-        item.style.maxWidth = itemWidth + "px";
-    })
-
-}
-
-menuSetup();
+//let words = [];
+//let $menuBtns = document.querySelectorAll(".bt-back");
+//let wordsCount = () => {
+//    $menuBtns.forEach((item, ind) => {
+//        words[ind] = item.textContent;
+//    })
+//}
+//wordsCount();
+//window.addEventListener("resize", menuSetup);
+//
+//function menuSetup() {
+//    let style = window.getComputedStyle(document.querySelector("body"), null).getPropertyValue('font-size');
+//    let fontSize = parseFloat(style);
+//    $menuBtns.forEach((item, ind) => {
+//        let itemWidth = words[ind].length * fontSize * 3;
+//        itemWidth = itemWidth - itemWidth * 0.45;
+//        item.style.maxWidth = itemWidth + "px";
+//    })
+//
+//}
+//
+//menuSetup();
